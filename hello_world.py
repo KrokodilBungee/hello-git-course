bd5623f2766cb022566fcbffaf9ccaa7f002b91a
Turkka <turkka.salonen@tuni.fi>
2023-07-31 19:02:31 +0300

    Remove unnessesary lines

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+
 def main():
     print("Hello world!")
 
 
 if __name__ == '__main__':
     main()
-=======
-print("Hello world!")
->>>>>>> b23547b242012497282910300f6e6c1d5a27df82
+
